d96553e26f78a607a55c4f29451edd93fdd463e1
bugNick <rlatjsgur000@naver.com>
2022-08-28 12:58:35 +0900

    study-react-api-integrage-useReducer

--- src/Users.js
+++ src/Users.js
@@ -1,31 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 
+// LOADING, SUCCESS, ERROR
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+  const fetchUsers = async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/"
+      );
+      dispatch({ type: "SUCCESS", data: response.data });
+    } catch (e) {
+      dispatch({ type: "ERROR", error: e });
+    }
+  };
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users/"
-      );
-      setUsers(response.data);
-    } catch (e) {
-      console.log(e.response.status);
-      setError(e);
-    }
-    setLoading(false);
-  };
-
+  const { loading, data: users, error } = state;
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
   if (!users) return null;
